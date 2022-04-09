202eb7f8b15aac8ef21a4fb7ee629e79eb8575ff
AnaMelisaGo <melisago.rockn@gmail.com>
2022-04-09 16:47:06 +0000

    Implement emailjs integration to contact page

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,1 +1,14 @@
-console.log('hello');
+function sendMail(contactForm) {
+    emailjs.send('melisa-resume', 'anamelisago-resume', {
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'project_request': contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log('Success!', response);
+        }, function(errorResponse) {
+            console.log('FAILED', errorResponse);
+        });
+    return false;
+}
