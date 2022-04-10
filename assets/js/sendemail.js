0f4e5253a48c5690662ba7b736b71170cbba5dbd
AnaMelisaGo <melisago.rockn@gmail.com>
2022-04-10 09:20:57 +0000

    Add message when submit button is clicked

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -6,10 +6,18 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            alert("Message sent! I'll get in touch with you as soon as possible. Thank you!")
-            console.log('Success!', response);
+            //alert("Message sent! I'll get in touch with you as soon as possible. Thank you!")
+            console.log('Success!', response.status);
+            
         }, function(errorResponse) {
             console.log('FAILED', errorResponse);
         });
     return false;
 }
+
+$('#submit').click(function() {
+    $('#conf-msg').html(`
+    <p class="text-center">
+        Message sent! I'll get in touch with you as soon as possible. Thank you ${$('#fullname').val()}!
+    </p>`)
+});
